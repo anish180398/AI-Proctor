d42560727740362e9f0a11421126f4495a85b63f
anish18398 <anish.s18398@pm.me>
2021-11-24 10:40:37 +0530

    combined file

--- main.py
+++ main.py
@@ -149,61 +149,61 @@ def mobileDetector(capture):
    
     
     #Open the weights file
-    wf = open(weights_file, 'rb')
-    major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
+        wf = open(weights_file, 'rb')
+        major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
 
-    #Define names of the Yolo layers (just for a reference)    
-    layers = ['yolo_darknet',
-            'yolo_conv_0',
-            'yolo_output_0',
-            'yolo_conv_1',
-            'yolo_output_1',
-            'yolo_conv_2',
-            'yolo_output_2']
+        #Define names of the Yolo layers (just for a reference)    
+        layers = ['yolo_darknet',
+                'yolo_conv_0',
+                'yolo_output_0',
+                'yolo_conv_1',
+                'yolo_output_1',
+                'yolo_conv_2',
+                'yolo_output_2']
 
-    for layer_name in layers:
-        sub_model = model.get_layer(layer_name)
-        for i, layer in enumerate(sub_model.layers):
-          
+        for layer_name in layers:
+            sub_model = model.get_layer(layer_name)
+            for i, layer in enumerate(sub_model.layers):
             
-            if not layer.name.startswith('conv2d'):
-                continue
                 
-            #Handles the special, custom Batch normalization layer
-            batch_norm = None
-            if i + 1 < len(sub_model.layers) and \
-                    sub_model.layers[i + 1].name.startswith('batch_norm'):
-                batch_norm = sub_model.layers[i + 1]
+                if not layer.name.startswith('conv2d'):
+                    continue
+                    
+                #Handles the special, custom Batch normalization layer
+                batch_norm = None
+                if i + 1 < len(sub_model.layers) and \
+                        sub_model.layers[i + 1].name.startswith('batch_norm'):
+                    batch_norm = sub_model.layers[i + 1]
 
-            filters = layer.filters
-            size = layer.kernel_size[0]
-            in_dim = layer.input_shape[-1]
+                filters = layer.filters
+                size = layer.kernel_size[0]
+                in_dim = layer.input_shape[-1]
 
-            if batch_norm is None:
-                conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
-            else:
-                # darknet [beta, gamma, mean, variance]
-                bn_weights = np.fromfile(
-                    wf, dtype=np.float32, count=4 * filters)
-                # tf [gamma, beta, mean, variance]
-                bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
+                if batch_norm is None:
+                    conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
+                else:
+                    # darknet [beta, gamma, mean, variance]
+                    bn_weights = np.fromfile(
+                        wf, dtype=np.float32, count=4 * filters)
+                    # tf [gamma, beta, mean, variance]
+                    bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
 
-            # darknet shape (out_dim, in_dim, height, width)
-            conv_shape = (filters, in_dim, size, size)
-            conv_weights = np.fromfile(
-                wf, dtype=np.float32, count=np.product(conv_shape))
-            # tf shape (height, width, in_dim, out_dim)
-            conv_weights = conv_weights.reshape(
-                conv_shape).transpose([2, 3, 1, 0])
+                # darknet shape (out_dim, in_dim, height, width)
+                conv_shape = (filters, in_dim, size, size)
+                conv_weights = np.fromfile(
+                    wf, dtype=np.float32, count=np.product(conv_shape))
+                # tf shape (height, width, in_dim, out_dim)
+                conv_weights = conv_weights.reshape(
+                    conv_shape).transpose([2, 3, 1, 0])
 
-            if batch_norm is None:
-                layer.set_weights([conv_weights, conv_bias])
-            else:
-                layer.set_weights([conv_weights])
-                batch_norm.set_weights(bn_weights)
+                if batch_norm is None:
+                    layer.set_weights([conv_weights, conv_bias])
+                else:
+                    layer.set_weights([conv_weights])
+                    batch_norm.set_weights(bn_weights)
 
-    assert len(wf.read()) == 0, 'failed to read all data'
-    wf.close()
+        assert len(wf.read()) == 0, 'failed to read all data'
+        wf.close()
     
     def draw_outputs(img, outputs, class_names):
     
